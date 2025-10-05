64a4911b27f9115af22803c792523a8746f64198
TahmidSWAD <swad2408@gmail.com>
2025-10-06 02:37:53 +0600

    Hello world

---
+++ essentials/c_language/hello/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int a = printf("Hello, World!\n"); // prints the string and returns the number of characters printed as function output
+    printf("Number of characters printed: %d\n", a); 
+    return 0;
+}
