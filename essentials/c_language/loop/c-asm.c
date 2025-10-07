41e47916c942eff68ba06398fda5ee0e5c5db340
TahmidSWAD <swad2408@gmail.com>
2025-10-08 01:53:41 +0600

    Loop

---
+++ essentials/c_language/loop/c-asm.c
@@ -0,0 +1,20 @@
+void swad(){
+    int n = 10;
+    do {
+        n--;
+    }while(n > 0);
+
+    // n = 10;
+    // while (n > 0)
+    // {   
+    //     n--;
+    // }
+    
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     n--;
+    // }
+    
+
+    return;
+}
