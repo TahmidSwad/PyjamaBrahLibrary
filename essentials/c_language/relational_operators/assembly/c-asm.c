8df508771deaf8997db747fd415a030e4365a162
TahmidSWAD <swad2408@gmail.com>
2025-10-12 20:45:32 +0600

    Relational operators

---
+++ essentials/c_language/relational_operators/assembly/c-asm.c
@@ -0,0 +1,14 @@
+int swad(){
+    
+    char a = 127;
+    char b = 3; 
+
+    int c;
+
+    c = a > b; // Greater than
+    c = a < b; // Less than
+    c = a >= b; // Greater than or equal to
+    c = a <= b; // Less than or equal to
+    c = a == b; // Equal to
+    c = a != b; // Not equal to
+}
---
+++ essentials/c_language/relational_operators/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // int a = 10;
+    // int b = 3;
+    char a = 3;
+    char b = 3;
+
+    printf("a > b = %d\n", a > b);   // Greater than
+    printf("a < b = %d\n", a < b);   // Less than
+    printf("a >= b = %d\n", a >= b); // Greater than or equal to
+    printf("a <= b = %d\n", a <= b); // Less than or equal to
+    printf("a == b = %d\n", a == b); // Equal to
+    printf("a != b = %d\n", a != b); // Not equal to
+    return 0;
+}
