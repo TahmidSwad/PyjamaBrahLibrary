080610407d4b7ac15fc96e2dd970255d9f9ced2e
TahmidSWAD <swad2408@gmail.com>
2025-10-12 19:36:42 +0600

    enum demo

---
+++ essentials/c_language/enum_demo/c-asm.c
@@ -0,0 +1,27 @@
+enum xyz {
+    a = 101,
+    b,
+    c = 5,
+};
+
+
+int swad(){
+    
+    // enum xyz {
+    //     a = 101,
+    //     b,
+    //     c = 5,
+    // };
+    
+    enum xyz pqr;
+
+    int c = a + b;
+
+    pqr = a;
+
+    int a = 100;
+    pqr = a;
+    pqr = b;
+    pqr = c;
+    return 0;
+}
